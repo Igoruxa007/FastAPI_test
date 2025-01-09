5856ee7bf4ca4b6fcb23b991cb023420853f32ba
igoruxa007 <igoruxa007@gmail.com>
2025-01-09 15:25:03 +0300

    Add APIRouter

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-app = FastAPI()
+app = FastAPI(
+    title="Test", openapi_url="/openapi.json"
+)
 
+api_router = APIRouter()
 
-@app.get("/")
-def read_root():
+@api_router.get("/", status_code=200)
+def read_root() -> dict:
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+@api_router.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None) -> dict:
     return {"item_id": item_id, "q": q}
+
+app.include_router(api_router)
