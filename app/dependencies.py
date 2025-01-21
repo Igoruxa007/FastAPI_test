f33205b95b9e4d2b716bd72b6db230667e4e0292
igoruxa007 <igoruxa007@gmail.com>
2025-01-21 15:01:00 +0300

    Mypy

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
+from collections.abc import Generator
+
 from app.db.base_class import SessionLocal
 
 
-def get_db():
+def get_db() -> Generator:
     db = SessionLocal()
     try:
         yield db
--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from __future__ import annotations
 from fastapi import APIRouter
 from fastapi import FastAPI
 
-from app.db.base_class import engine
-from app.models import user
 
 app = FastAPI(
     title='Test', openapi_url='/openapi.json',
@@ -12,12 +10,15 @@ app = FastAPI(
 
 api_router = APIRouter()
 
-user.Base.metadata.create_all(bind=engine)
-
 
 @api_router.get('/', status_code=200)
 def read_root() -> dict:
     return {'Hello': 'World'}
+
+
+@api_router.get('/users/{user_id}')
+def read_user(user_id: int, q: str | None = None) -> dict:
+    return {'item_id': user_id, 'q': q}
 
 
 @api_router.get('/items/{item_id}')
